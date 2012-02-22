ec5d5e6b27ce7505052841da61123820913f46aa
KellyLSB <kellylsbkr@gmail.com>
2012-02-22 14:45:10 -0700

    rawr

---
+++ _bundle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Bundles\Taxonomy;
+use Exception;
+use e;
+
+class Bundle {
+
+	
+
+}
---
+++ extend/sql/library/extensions/taxonomy.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Bundles\SQL\Extensions;
+use Bundles\SQL\ListObj;
+use Bundles\SQL\Model;
+use Exception;
+use e;
+
+class Taxonomy {
+
+	public function _buildTable($tableName) {
+		e::$sql->architect("\$tags $tableName", array(
+			"id" => '_suppress',
+			"created_timestamp" => '_suppress',
+			"updated_timestamp" => array(
+				'Type' => 'timestamp',
+				'Null' => 'YES',
+				'Key' => '',
+				'Default' => 'CURRENT_TIMESTAMP',
+				'Extra' => 'on update CURRENT_TIMESTAMP'
+			),
+			"model" => array(
+				'Type' => 'string',
+				'Key' => 'PRI'
+			),
+			"model-id" => array(
+				'Type' => 'string',
+				'Key' => 'PRI'
+			),
+			"flags" => 'number'
+		));
+	}
+
+	public function modelTags(Model $model) {
+		return e::taxonomy($model);
+	}
+
+	public function listHasTag(ListObj $list, $model) {
+		$args = func_get_args();
+		array_shift($args);
+
+		foreach($args as $arg)
+			$list->condition('model', $arg);
+
+		return $list;
+	}
+
+}
+
+$list->_->taxonomy->hasTag('members.member', 'momentum.project');
