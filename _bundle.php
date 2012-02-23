53c3e52fdbcafd5850cca4668d0c41a50a347deb
KellyLSB <kellylsbkr@gmail.com>
2012-02-23 11:15:58 -0700

    taxonomy

--- _bundle.php
+++ _bundle.php
@@ -10,7 +10,8 @@ class Bundle extends SQLBundle {
 	public function __callExtend($func, $args) {
 		static $run = 0;
 		if(($func == 'getTag' && !is_numeric($slug = $args[0])) || $run == 1) {
-			list($category, $name) = explode(':', $slug);
+			if(strpos($slug, ':') === false) throw new Exception("No category was specified in `getTag`");
+			list($category, $name) = explode(':', $slug, 2);
 			$result = e::$sql->query("SELECT * FROM `taxonomy.tag` WHERE `name` = '$name' AND `category` = '$category'")->row();
 			$run = 1; if($result) return $this->getTag($result);
 		}
