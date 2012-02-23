198b84100d1602d2318ddf89cf218fdf8f950e85
KellyLSB <kellylsbkr@gmail.com>
2012-02-23 09:37:56 -0700

    added generic tagging

--- _bundle.php
+++ _bundle.php
@@ -1,11 +1,20 @@
 <?php
 
 namespace Bundles\Taxonomy;
+use Bundles\SQL\SQLBundle;
 use Exception;
 use e;
 
-class Bundle {
+class Bundle extends SQLBundle {
 
-	
+	public function __callExtend($func, $args) {
+		static $run = 0;
+		if(($func == 'getTag' && !is_numeric($slug = $args[0])) || $run == 1) {
+			list($category, $name) = explode(':', $slug);
+			$result = e::$sql->query("SELECT * FROM `taxonomy.tag` WHERE `name` = '$name' AND `category` = '$category'")->row();
+			$run = 1; if($result) return $this->getTag($result);
+		}
+		throw new e\AutoLoadException;
+	}
 
 }
