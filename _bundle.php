6c9562cc046c695237cca573ba5381cd4d54371a
Kelly Becker <kbecker@kellybecker.me>
2012-03-21 13:10:20 -0600

    docoumentation

--- _bundle.php
+++ _bundle.php
@@ -5,30 +5,86 @@ use Bundles\SQL\SQLBundle;
 use Exception;
 use e;
 
+/**
+ * Taxonomy Bundle
+ * @author Kelly Becker
+ */
 class Bundle extends SQLBundle {
 
+	/**
+	 * Extension for getTag to allow getting tags by name and category
+	 * @author Kelly Becker
+	 */
 	public function __callExtend($func, $args) {
+
+		/**
+		 * Create a variable to determine if were on stage one or two
+		 * @author Kelly Becker
+		 */
 		static $run = 0;
 
+		/**
+		 * If we are getting a tag, the argument passed is not numeric, and we havent been here before
+		 * @author Kelly Becker
+		 */
 		if(($func == 'getTag' && !is_numeric($slug = $args[0])) && $run < 1) {
+
+			/**
+			 * If we dont detect a category then use "default"
+			 * @author Kelly Becker
+			 */
 			if(strpos($slug, ':') === false) {
 				$category = 'default';
 				$name = $slug;
 			}
+
+			/**
+			 * Since we have detected a category seperate it out
+			 * @author Kelly Becker
+			 */
 			else list($category, $name) = explode(':', $slug, 2);
+
+			/**
+			 * Run a query to find the requested tag
+			 * @author Kelly Becker
+			 */
 			$result = e::$sql->query("SELECT * FROM `taxonomy.tag` WHERE `name` = '$name' AND `category` = '$category'")->row();
+
+			/**
+			 * Since we got this far we dont want to go through this again if we fail
+			 */
 			$run++;
 
+			/**
+			 * Get the model of the tag using the result of the query above
+			 * @author Kelly Becker
+			 */
 			if($result) return $this->getTag($result);
+
+			/**
+			 * If no tag was found create one and return it
+			 * @author Kelly Becker
+			 */
 			else {
 				$tag = $this->newTag();
 				$tag->category = $category;
 				$tag->name = $name;
 				$tag->save();
+				$run = 0;
 				return $tag;
 			}
 		}
+
+		/**
+		 * Since nothing was detected set run to zero so we can use the above script again
+		 * @author Kelly Becker
+		 */
 		$run = 0;
+
+		/**
+		 * Throw an exception telling the call method to run normally
+		 * @author Kelly Becker
+		 */
 		throw new e\AutoLoadException;
 	}
 
--- extend/sql/library/extensions/taxonomy.php
+++ extend/sql/library/extensions/taxonomy.php
@@ -7,8 +7,17 @@ use MapException;
 use Exception;
 use e;
 
+/**
+ * Taxonomy SQL Extension
+ * @author Kelly Becker
+ * @author Nate Ferrero
+ */
 class Taxonomy {
 
+	/**
+	 * Event to build the connections table on sql build
+	 * @author Kelly Becker
+	 */
 	public function _buildTable($tableName) {
 		e::$sql->architect("\$tags $tableName", array(
 			'fields' => array(
@@ -30,24 +39,53 @@ class Taxonomy {
 		));
 	}
 
+	/**
+	 * Add an additional field to the table
+	 * @author Kelly Becker
+	 */
 	public function _tableStructure($table, &$structure) {
 		$structure['fields']['$tag_count'] = 'number';
 	}
 
+	/**
+	 * Add a tag by model map or tag name
+	 * @author Kelly Becker
+	 */
 	public function modelAddTag(Model $model, $map = false, $priority = 0) {
+
+		/**
+		 * Generate the name of the table were gonna use
+		 * @author Kelly Becker
+		 */
 		$tagTable = "\$tags ".$model->__getTable();
 
+		/**
+		 * If a model instace was passed do nothing, if a map was passed convert it to a model
+		 * @author Kelly Becker
+		 */
 		try {
 			if($map instanceof Model);
 			else $map = e::map($map);
 		}
 
+		/**
+		 * If the map that was passed was not a valid map it must be a tag instead. Convert it to a tag
+		 * @author Kelly Becker
+		 */
 		catch(MapException $e) {
 			$map = e::$taxonomy->getTag($map);
 		}
 
+		/**
+		 * See if a tag already exists
+		 * @author Kelly Becker
+		 */
 		$q = e::$sql->query("SELECT * FROM `$tagTable` WHERE `owner` = '$model->id' AND `model` = '".$map->__map('bundlename')."' AND `model-id` = '$map->id'")->row();
 
+		/**
+		 * If the tag does not exist yet create it on the table
+		 * @author Kelly Becker
+		 */
 		if(!$q) e::$sql->insert($tagTable, array(
 			'model' => $map->__map('bundlename'),
 			'model-id' => $map->id,
@@ -58,87 +96,205 @@ class Taxonomy {
 		return true;
 	}
 
+	/**
+	 * Remove a tag by model or tag name
+	 * @author Kelly Becker
+	 */
 	public function modelRemoveTag(Model $model, $map = false) {
+
+		/**
+		 * Generate the name of the table were gonna use
+		 * @author Kelly Becker
+		 */
 		$tagTable = "\$tags ".$model->__getTable();
 
+		/**
+		 * If a model instace was passed do nothing, if a map was passed convert it to a model
+		 * @author Kelly Becker
+		 */
 		try {
 			if($map instanceof Model);
 			else $map = e::map($map);
 		}
 
+		/**
+		 * If the map that was passed was not a valid map it must be a tag instead. Convert it to a tag
+		 * @author Kelly Becker
+		 */
 		catch(MapException $e) {
 			$map = e::$taxonomy->getTag($map);
 		}
 
+		/**
+		 * Delete the tag connection from the tag table
+		 * @author Kelly Becker
+		 */
 		$q = e::$sql->query("DELETE FROM `$tagTable` WHERE `owner` = '$model->id' AND `model` = '".$map->__map('bundlename')."' AND `model-id` = '$map->id'")->row();
 
 		return true;
 	}
 
+	/**
+	 * Check if a tag exists on the model
+	 * @author Kelly Becker
+	 */
 	public function modelHasTag(Model $model, $map = false) {
+
+		/**
+		 * Check to ensure that a map was passed
+		 * @author Kelly Becker
+		 */
 		if(!$map) throw new Exception("No model Map was passed");
+
+		/**
+		 * Generate the name of the table were gonna use
+		 * @author Kelly Becker
+		 */
 		$tagTable = "\$tags ".$model->__getTable();
 
+		/**
+		 * If a model instace was passed do nothing, if a map was passed convert it to a model
+		 * @author Kelly Becker
+		 */
 		try {
 			if($map instanceof Model);
 			else $map = e::map($map);
 		}
 
+		/**
+		 * If the map that was passed was not a valid map it must be a tag instead. Convert it to a tag
+		 * @author Kelly Becker
+		 */
 		catch(MapException $e) {
 			$map = e::$taxonomy->getTag($map);
 		}
 
+		/**
+		 * Check if we get any results
+		 * @author Kelly Becker
+		 */
 		$q = e::$sql->query("SELECT * FROM `$tagTable` WHERE `owner` = '$model->id' AND `model` = '".$map->__map('bundlename')."' AND `model-id` = '$map->id'")->row();
 
+		/**
+		 * If there was a result return true, else return false
+		 * @author Kelly Becker
+		 */
 		if(!$q) return false;
 		else return true;
 	}
 
+	/**
+	 * List all the connected tags on the model
+	 * @author Kelly Becker
+	 */
 	public function modelListTags(Model $model) {
+
+		/**
+		 * Generate the name of the table were gonna use
+		 * @author Kelly Becker
+		 */
 		$tagTable = "\$tags ".$model->__getTable();
 
+		/**
+		 * Get and return all the tags on the model
+		 * @author Kelly Becker
+		 */
 		return e::$sql->query("SELECT * FROM `$tagTable` WHERE `owner` = '$model->id' ORDER BY `updated_timestamp` DESC")->all();
 	}
 
+	/**
+	 * Prioritize a tag
+	 * @author Kelly Becker
+	 */
 	public function modelPrioritizeTag(Model $model, $map = false, $priority = 0, $up_down = false) {
+
+		/**
+		 * Check to ensure that a map was passed
+		 * @author Kelly Becker
+		 */
 		if(!$map) throw new Exception("No model Map was passed");
+
+		/**
+		 * Generate the name of the table were gonna use
+		 * @author Kelly Becker
+		 */
 		$tagTable = "\$tags ".$model->__getTable();
 
+
+		/**
+		 * If a model instace was passed do nothing, if a map was passed convert it to a model
+		 * @author Kelly Becker
+		 */
 		try {
 			if($map instanceof Model);
 			else $map = e::map($map);
 		}
 
+		/**
+		 * If the map that was passed was not a valid map it must be a tag instead. Convert it to a tag
+		 * @author Kelly Becker
+		 */
 		catch(MapException $e) {
 			$map = e::$taxonomy->getTag($map);
 		}
 
-
+		/**
+		 * Are we are changin a priority by one
+		 * @author Kelly Becker
+		 */
 		if($up_down) {
 			$q = e::$sql->query("SELECT * FROM `$tagTable` WHERE `owner` = '$model->id' AND `model` = '".$map->__map('bundlename')."' AND `model-id` = '$map->id'")->row();
 			if($priority) $priority = ((float) $q['priority']) + 1;
 			else $priority = ((float) $q['priority']) - 1;
 		}
 
+		/**
+		 * Set Priority on the tag
+		 * @author Kelly Becker
+		 */
 		$q = e::$sql->query("UPDATE `$tagTable` SET `priority` = '$priority' WHERE `owner` = '$model->id' AND `model` = '".$map->__map('bundlename')."' AND `model-id` = '$map->id'")->row();
 
+		/**
+		 * If we succeeded return true, else return false
+		 * @author Kelly Becker
+		 */
 		if(!$q) return false;
 		else return true;
 	}
 
+	/**
+	 * Filtering function for lists
+	 * @author Kelly Becker
+	 */
 	public function listHasTag(ListObj $list, $map = false) {
 
+		/**
+		 * Start by joining the tags table with the table of model
+		 * @author Kelly Becker
+		 */
 		$list->join('LEFT', "\$tags $list->_table", "`$list->_table`.`id` = `\$tags $list->_table`.`owner`");
 
+		/**
+		 * If a model instace was passed do nothing, if a map was passed convert it to a model
+		 * @author Kelly Becker
+		 */
 		try {
 			if($map instanceof Model);
 			else $map = e::map($map);
 		}
 
+		/**
+		 * If the map that was passed was not a valid map it must be a tag instead. Convert it to a tag
+		 * @author Kelly Becker
+		 */
 		catch(MapException $e) {
 			$map = e::$taxonomy->getTag($map);
 		}
 
+		/**
+		 * I know we just got ourselves a model from a map but lets get the map back
+		 * @author Kelly Becker
+		 */
 		try {
 			if($map instanceof Model)
 				$map = $map->__map();
@@ -146,15 +302,32 @@ class Taxonomy {
 		}
 		catch(Exception $e) {}
 
+		/**
+		 * If you are requesting an id with the map then break it up
+		 * @author Kelly Becker
+		 */
 		if(strpos($map, ':') !== false) {
 			list($model, $id) = explode(':', $map);
 			$list->condition("`\$tags $list->_table`.`model` =", $model);
 			$list->condition("`\$tags $list->_table`.`model-id` =", $id);
 		}
+
+		/**
+		 * Id no id is requested then just list off the ones with tags to the model type
+		 * @author Kelly Becker
+		 */
 		else $list->condition("`\$tags $list->_table`.`model` =", $arg);
 
+		/**
+		 * Set the order to be by tag priortiy
+		 * @author Kelly Becker
+		 */
 		$list->order("`\$tags $list->_table`.`priority`", ASC);
 
+		/**
+		 * Return the list object
+		 * @author Kelly Becker
+		 */
 		return $list;
 	}
 
